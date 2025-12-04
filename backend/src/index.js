df9a7cbad14fe5d5f292a44769f35ee9f75a3b42
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-12-04 10:17:14 +0530

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 //initialize socket.io server
 export const io = new Server(server, {
    cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.FRONTEND_URL,
       methods: ["GET", "POST"],
       credentials: true,
    },
@@ -50,7 +50,7 @@ app.use(cookieParser());
 
 app.use(
    cors({
-      origin: process.env.FRONTEND_URL || "http://localhost:5173",
+      origin: process.env.FRONTEND_URL,
       credentials: true,
    })
 );
